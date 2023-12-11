3ae95580ec5d614c997bb26b3b4b7c18972c632b
Savchenko <savchenko.ra@yandex.ru>
2023-12-11 17:21:13 +0300

    Поправил названия DTO

---
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateAdDTO.java
@@ -0,0 +1,11 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateOrUpdateAdDTO {
+
+    private String title;
+    private String price;
+    private String description;
+}
