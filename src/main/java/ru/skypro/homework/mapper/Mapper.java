aa082316ecb2abf341e52704019c9911ae3cbb8e
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-12-17 13:35:18 +0300

    7/feat - методы в маппере для преобразования Comment - CommentDTO и наоборот

--- src/main/java/ru/skypro/homework/mapper/Mapper.java
+++ src/main/java/ru/skypro/homework/mapper/Mapper.java
@@ -2,7 +2,9 @@ package ru.skypro.homework.mapper;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+import ru.skypro.homework.dto.CommentDTO;
 import ru.skypro.homework.dto.UserDTO;
+import ru.skypro.homework.entity.Comment;
 import ru.skypro.homework.entity.User;
 
 @Component
@@ -15,5 +17,13 @@ public class Mapper {
 
     public static User toUser(UserDTO userDto) {
         return modelMapper.map(userDto, User.class);
+    }
+
+    public static CommentDTO toCommentDTO(Comment comment) {
+        return modelMapper.map(comment, CommentDTO.class);
+    }
+
+    public static Comment toComment(CommentDTO commentDTO) {
+        return modelMapper.map(commentDTO, Comment.class);
     }
 }
