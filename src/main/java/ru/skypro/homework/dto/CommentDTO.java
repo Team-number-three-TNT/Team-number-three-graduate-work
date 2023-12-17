38037c8e0f625ecdc5e69e861fcb3906237a77bb
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-12-17 12:32:17 +0300

    7/chore - убрал ненужный импорт

--- src/main/java/ru/skypro/homework/dto/CommentDTO.java
+++ src/main/java/ru/skypro/homework/dto/CommentDTO.java
@@ -3,7 +3,6 @@ package ru.skypro.homework.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class CommentDTO {
