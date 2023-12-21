35be710fdf58f9469049253edfbf75659ba0cd9e
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-12-21 18:41:26 +0300

    10. Переименование исключения и добавление еще одного: AdNotFoundException

--- src/main/java/ru/skypro/homework/utils/GlobalExceptionHandler.java
+++ src/main/java/ru/skypro/homework/utils/GlobalExceptionHandler.java
@@ -5,13 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import ru.skypro.homework.exception.CommentDoesntExistException;
+import ru.skypro.homework.exception.CommentNotFoundException;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(CommentDoesntExistException.class)
+    @ExceptionHandler(CommentNotFoundException.class)
     public ResponseEntity<String> handleCommentDoesntExistException(Exception e) {
         log.warn(e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
