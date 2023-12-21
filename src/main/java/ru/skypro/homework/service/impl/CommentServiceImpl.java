d5f0e1d02dcb3b1ed555fed22ac484a47862e2e0
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-12-21 18:43:13 +0300

    10. Основная логика методов CommentServiceImpl и необходимые изменения в других классах

--- src/main/java/ru/skypro/homework/service/impl/CommentServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/CommentServiceImpl.java
@@ -1,32 +1,94 @@
 package ru.skypro.homework.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.CommentDTO;
 import ru.skypro.homework.dto.CommentsDTO;
 import ru.skypro.homework.dto.CreateOrUpdateCommentDTO;
+import ru.skypro.homework.entity.Comment;
+import ru.skypro.homework.exception.AdNotFoundException;
+import ru.skypro.homework.exception.CommentNotFoundException;
+import ru.skypro.homework.mapper.Mapper;
+import ru.skypro.homework.repository.AdRepository;
+import ru.skypro.homework.repository.CommentRepository;
 import ru.skypro.homework.service.CommentService;
 
-import java.util.List;
-
 @Service
+@RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
+
+    private final Mapper mapper;
+    private final CommentRepository commentRepository;
+    private final AdRepository adRepository;
+
+    /**
+     * Возвращет все комментарии для конкретного объявления
+     * @param adId Id объявления
+     * @return CommentsDTO
+     * @see CommentsDTO
+     */
     @Override
-    public List<CommentsDTO> getCommentsForAd(int adId) {
-        return null;
+    public CommentsDTO getCommentsForAd(int adId) {
+        return mapper.toCommentsDTO(commentRepository.findCommentsByAdId(adId));
     }
 
+    /**
+     * Добавление нового комментария к объявлению
+     * @param adId Id объявления
+     * @param createOrUpdateCommentDTO DTO объект, представляющий новый комментарий
+     * @return CommentDTO нового комментария
+     * @throws AdNotFoundException, если объявления с переданным Id не существует
+     */
     @Override
     public CommentDTO addCommentToAd(int adId, CreateOrUpdateCommentDTO createOrUpdateCommentDTO) {
-        return null;
+        Comment newComment = new Comment();
+        newComment.setCreatedAt(System.currentTimeMillis());
+        newComment.setText(createOrUpdateCommentDTO.getText());
+        newComment.setAd(adRepository.findById(adId)
+                .orElseThrow(() -> new AdNotFoundException("Объявление с Id: " + adId + " не найдено")));
+        //new.Comment.setUser(getCurrentUser());
+        return mapper.toCommentDTO(commentRepository.save(newComment));
     }
 
+    /**
+     * Удаление ранее опубликованного комментария, принадлежащего текущему пользователю
+     * @param adId Id объявления
+     * @param commentId Id комменатрия
+     * @throws CommentNotFoundException, если комментария с переданным Id не существует
+     */
     @Override
     public void deleteComment(int adId, int commentId) {
-
+        //проверка, что комментария принадлежит данному пользователю
+        commentRepository.delete(findByCommentId(commentId));
     }
 
+    /**
+     * Обновление текста комментария пользователя
+     * @param adId id объявления, к которому был опубликован комментарий
+     * @param commentId id комментария
+     * @param createOrUpdateCommentDTO DTO объект, представляющий обновленный комментарий
+     * @return обновленный CommentDTO
+     * @throws CommentNotFoundException, если комментария с переданным Id не существует
+     */
     @Override
-    public CommentDTO updateCommentText(int adId, int commentId, CreateOrUpdateCommentDTO createOrUpdateCommentDTO) {
-        return null;
+    public CommentDTO updateCommentText(int adId,
+                                        int commentId,
+                                        CreateOrUpdateCommentDTO createOrUpdateCommentDTO) {
+        //проверка, что комментария принадлежит данному пользователю
+        Comment commentToUpdate = findByCommentId(commentId);
+        commentToUpdate.setText(createOrUpdateCommentDTO.getText());
+        commentToUpdate.setCreatedAt(System.currentTimeMillis());
+        return mapper.toCommentDTO(commentRepository.save(commentToUpdate));
+    }
+
+    private Comment findByCommentId(int commentId) {
+        checkIfCommentExist(commentId);
+        return commentRepository.findById(commentId).get();
+    }
+
+    private void checkIfCommentExist(int commentId) {
+        if (!commentRepository.existsById(commentId)) {
+            throw new CommentNotFoundException("Комментария с id: " + commentId + " не существует");
+        }
     }
 }
