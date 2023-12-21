5c1ccbdfbcc0a3b1cce7baf40ecfe61739f5b524
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-12-21 18:41:14 +0300

    10. Переименование исключения и добавление еще одного: AdNotFoundException

--- src/main/java/ru/skypro/homework/exception/CommentDoesntExistException.java
+++
@@ -1,7 +0,0 @@
-package ru.skypro.homework.exception;
-
-public class CommentDoesntExistException extends RuntimeException {
-    public CommentDoesntExistException(String message) {
-        super(message);
-    }
-}
