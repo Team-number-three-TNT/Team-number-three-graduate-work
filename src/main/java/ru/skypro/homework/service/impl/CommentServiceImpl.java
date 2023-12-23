6bedbfff7650e0f8514b65f482d5541f20ecb6c9
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-12-23 20:59:36 +0300

    10/feat: метод, возвращающий текущего пользователя

--- src/main/java/ru/skypro/homework/service/impl/CommentServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/CommentServiceImpl.java
@@ -1,16 +1,21 @@
 package ru.skypro.homework.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.CommentDTO;
 import ru.skypro.homework.dto.CommentsDTO;
 import ru.skypro.homework.dto.CreateOrUpdateCommentDTO;
 import ru.skypro.homework.entity.Comment;
+import ru.skypro.homework.entity.User;
 import ru.skypro.homework.exception.AdNotFoundException;
 import ru.skypro.homework.exception.CommentNotFoundException;
 import ru.skypro.homework.mapper.Mapper;
 import ru.skypro.homework.repository.AdRepository;
 import ru.skypro.homework.repository.CommentRepository;
+import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.service.CommentService;
 
 @Service
@@ -20,6 +25,7 @@ public class CommentServiceImpl implements CommentService {
     private final Mapper mapper;
     private final CommentRepository commentRepository;
     private final AdRepository adRepository;
+    private final UserRepository userRepository;
 
     /**
      * Возвращет все комментарии для конкретного объявления
@@ -46,7 +52,7 @@ public class CommentServiceImpl implements CommentService {
         newComment.setText(createOrUpdateCommentDTO.getText());
         newComment.setAd(adRepository.findById(adId)
                 .orElseThrow(() -> new AdNotFoundException("Объявление с Id: " + adId + " не найдено")));
-        //new.Comment.setUser(getCurrentUser());
+        newComment.setUser(getCurrentUser());
         return mapper.toCommentDTO(commentRepository.save(newComment));
     }
 
@@ -79,6 +85,16 @@ public class CommentServiceImpl implements CommentService {
         commentToUpdate.setText(createOrUpdateCommentDTO.getText());
         commentToUpdate.setCreatedAt(System.currentTimeMillis());
         return mapper.toCommentDTO(commentRepository.save(commentToUpdate));
+    }
+
+    /**
+     * Метод, который возвращает текущего пользователя
+     * @return User
+     * @see User
+     */
+    private User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findByEmail(((UserDetails) authentication.getPrincipal()).getUsername());
     }
 
     private Comment findByCommentId(int commentId) {
