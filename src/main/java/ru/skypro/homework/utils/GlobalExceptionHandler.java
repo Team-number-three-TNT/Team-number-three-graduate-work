3dce00db945793ece196c1d979e3857c92d780ac
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-12-24 18:22:03 +0300

    7/fix: исключение, если аватар пользователя не найден

--- src/main/java/ru/skypro/homework/utils/GlobalExceptionHandler.java
+++ src/main/java/ru/skypro/homework/utils/GlobalExceptionHandler.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import ru.skypro.homework.exception.AdNotFoundException;
-import ru.skypro.homework.exception.CommentNotFoundException;
-import ru.skypro.homework.exception.UserNotAuthorizedException;
-import ru.skypro.homework.exception.UserNotFoundException;
+import ru.skypro.homework.exception.*;
 
 @ControllerAdvice
 @Slf4j
@@ -36,5 +33,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleUserNotAuthorizedException(Exception e) {
         log.warn(e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(UserHasNotImageException.class)
+    public ResponseEntity<String> handleUserHasNotImageException(Exception e) {
+        log.warn(e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
