225e5c6207e58bc3618a1fdcbabe29eb8aaa5f0e
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-12-24 20:26:15 +0300

    7/feat: удаление метода регистрации, который был перенесен в ресурс register

--- src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
@@ -1,47 +1,23 @@
 package ru.skypro.homework.service.impl;
 
-import org.springframework.security.core.userdetails.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
-import ru.skypro.homework.dto.RegisterDTO;
 import ru.skypro.homework.service.AuthService;
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
-    private final UserDetailsManager manager;
+    private final UserDetailsService userDetailsService;
     private final PasswordEncoder encoder;
 
-    public AuthServiceImpl(UserDetailsManager manager,
-                           PasswordEncoder passwordEncoder) {
-        this.manager = manager;
-        this.encoder = passwordEncoder;
-    }
 
     @Override
     public boolean login(String userName, String password) {
-        if (!manager.userExists(userName)) {
-            return false;
-        }
-        UserDetails userDetails = manager.loadUserByUsername(userName);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
         return encoder.matches(password, userDetails.getPassword());
     }
-
-    @Override
-    public boolean register(RegisterDTO registerDTO) {
-        if (manager.userExists(registerDTO.getUsername())) {
-            return false;
-        }
-        manager.createUser(
-                User.builder()
-                        .passwordEncoder(this.encoder::encode)
-                        .password(registerDTO.getPassword())
-                        .username(registerDTO.getUsername())
-                        .roles(registerDTO.getRole().name())
-                        .build());
-        return true;
-    }
-
 }
