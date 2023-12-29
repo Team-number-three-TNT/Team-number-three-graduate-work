ed54dfb1f6e9d187c8f776b49f5f494a29e11380
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-12-29 23:01:38 +0300

    17/feat: тесты для CommentServiceImpl (кроме update)

---
+++ src/test/java/ru/skypro/homework/service/impl/CommentServiceImplTest.java
@@ -0,0 +1,154 @@
+package ru.skypro.homework.service.impl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.skypro.homework.dto.CommentsDTO;
+import ru.skypro.homework.dto.CreateOrUpdateCommentDTO;
+import ru.skypro.homework.entity.Comment;
+import ru.skypro.homework.entity.User;
+import ru.skypro.homework.exception.AdNotFoundException;
+import ru.skypro.homework.exception.CommentNotFoundException;
+import ru.skypro.homework.exception.UserNotAuthorizedException;
+import ru.skypro.homework.exception.UserNotFoundException;
+import ru.skypro.homework.mapper.CommentMapper;
+import ru.skypro.homework.repository.AdRepository;
+import ru.skypro.homework.repository.CommentRepository;
+import ru.skypro.homework.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static ru.skypro.homework.utils.Constants.*;
+import static ru.skypro.homework.utils.EntityDtoFactory.*;
+
+@ExtendWith(MockitoExtension.class)
+class CommentServiceImplTest {
+
+    @Mock
+    CommentRepository commentRepository;
+
+    @Mock
+    SecurityContext securityContext;
+
+    @Mock
+    Authentication authentication;
+
+    @Mock
+    AdRepository adRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    CommentMapper commentMapper;
+
+    @InjectMocks
+    CommentServiceImpl out;
+
+    @Test
+    @DisplayName("'getCommentsForAd' should return CommentsDTO with the corresponding List of comments that were stored in DB and also right 'count' value")
+    void testGetCommentsForAd() {
+        Comment comment = getComment();
+        when(commentMapper.toDto(any(Comment.class))).thenReturn(getCommentDto());
+        when(commentRepository.findCommentsByAdId(ID1)).thenReturn(List.of(comment));
+        when(commentMapper.toCommentsDTO(List.of(comment))).thenReturn(new CommentsDTO(1, List.of(getCommentDto())));
+
+        CommentsDTO expected = new CommentsDTO(1, List.of(commentMapper.toDto(comment)));
+        assertThat(out.getCommentsForAd(ID1)).isEqualTo(expected);
+        assertThat(out.getCommentsForAd(ID1).getCount()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("'addCommentToAd' should return CommentDTO with 'text' field which is equal to given CreateOrUpdateComment DTO")
+    void testAddCommentToAd() {
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(authentication.getPrincipal()).thenReturn(getUserPrincipal());
+        when(adRepository.findById(ID1)).thenReturn(Optional.of(getAd()));
+        when(userRepository.findByEmail(EMAIL)).thenReturn(Optional.of(getUser()));
+        when(commentRepository.save(any(Comment.class))).thenReturn(getComment());
+        when(commentMapper.toDto(getComment())).thenReturn(getCommentDto());
+
+        CreateOrUpdateCommentDTO createOrUpdateCommentDTO = new CreateOrUpdateCommentDTO();
+        createOrUpdateCommentDTO.setText(COMMENT_TEXT);
+        assertThat(out.addCommentToAd(ID1, createOrUpdateCommentDTO).getText()).isEqualTo(COMMENT_TEXT);
+    }
+
+    @Test
+    @DisplayName("'addCommentToAd' should throw AdNotFoundException if DB does not contain the Ad of the given Id")
+    void addCommentToAd_shouldThrow() {
+        when(adRepository.findById(ID1)).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> out.addCommentToAd(ID1, new CreateOrUpdateCommentDTO()))
+                .isInstanceOf(AdNotFoundException.class)
+                .hasMessageContaining("Объявление с Id: " + ID1 + " не найдено");
+    }
+
+//    @Test
+//    @DisplayName("'deleteComment' should been executed correctly if user is the author of comment")
+//    void deleteComment() {
+//        when(securityContext.getAuthentication()).thenReturn(authentication);
+//        SecurityContextHolder.setContext(securityContext);
+//        when(authentication.getPrincipal()).thenReturn(getUserPrincipal());
+//        when(userRepository.findById(ID1)).thenReturn(Optional.of(getUser()));
+//    }
+
+    @Test
+    @DisplayName("'deleteComment' should throw CommentNotFoundException if DB does not contain the Comment of the given Id")
+    void deleteComment_shouldThrow1() {
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(authentication.getPrincipal()).thenReturn(getUserPrincipal());
+        when(userRepository.findByEmail(EMAIL)).thenReturn(Optional.of(getUser()));
+        when(commentRepository.findAuthorIdById(ID1)).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> out.deleteComment(ID1, ID1))
+                .isInstanceOf(CommentNotFoundException.class)
+                .hasMessageContaining("Комментарий с id: " + ID1 + " не найден");
+    }
+
+    @Test
+    @DisplayName("'deleteComment' should throw UserNotFoundException if DB does not contain the User of the given Id")
+    void deleteComment_shouldThrow2() {
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(authentication.getPrincipal()).thenReturn(getUserPrincipal());
+        when(userRepository.findById(ID1)).thenReturn(Optional.empty());
+        when(userRepository.findByEmail(EMAIL)).thenReturn(Optional.of(getUser()));
+        when(commentRepository.findAuthorIdById(ID1)).thenReturn(Optional.of(ID1));
+        when(userRepository.findById(ID1)).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> out.deleteComment(ID1, ID1))
+                .isInstanceOf(UserNotFoundException.class)
+                .hasMessageContaining("Пользователь, написавший комментария с id: " + ID1 + " не найден");
+    }
+
+    @Test
+    @DisplayName("'deleteComment' should throw UserNotAuthorizedException if User is not the author of the comment")
+    void deleteComment_shouldThrow3() {
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(authentication.getPrincipal()).thenReturn(getUserPrincipal());
+        when(userRepository.findById(ID1)).thenReturn(Optional.of(getUser()));
+        when(userRepository.findByEmail(EMAIL)).thenReturn(Optional.of(getUser()));
+        when(commentRepository.findAuthorIdById(ID1)).thenReturn(Optional.of(ID1));
+        User commentAuthor = getUser();
+        commentAuthor.setId(ID2);
+        when(userRepository.findById(ID1)).thenReturn(Optional.of(commentAuthor));
+        assertThatThrownBy(() -> out.deleteComment(ID1, ID1))
+                .isInstanceOf(UserNotAuthorizedException.class)
+                .hasMessageContaining("У пользователя c id: " + ID1 + " недостаточно прав для удаления комментария: " + ID1);
+    }
+
+    @Test
+    void updateCommentText() {
+    }
+}
